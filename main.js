cb58c39685915dd92fbb561f2d53d94e2a35be15
bagortechnology <hello.bagortech@gmail.com>
2023-03-25 17:08:52 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let form = document.getElementById("form");
 let textInput = document.getElementById("textInput");
+let msg = document.getElementById("msg");
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
