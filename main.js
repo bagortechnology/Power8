2f9abaedb3a07226f6db26c8c2336a1938b8b849
bagortechnology <hello.bagortech@gmail.com>
2023-03-25 11:55:30 +0800

    modified:   main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
 
-form.addEventListener("Submit",(e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("button clicked");
-
+  
     formValidation();
-});
+  });
 
-let formValidation = () => {
+  let formValidation = () => {
     if (input.value === "") {
       msg.innerHTML = "Post cannot be blank";
       console.log("failure");
