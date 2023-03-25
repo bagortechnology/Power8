0b15752d8013ae8eea62d1fa1945df61f8964de5
bagortechnology <hello.bagortech@gmail.com>
2023-03-25 12:01:45 +0800

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
-let form = document.getElementById("form");
-let input = document.getElementById("input");
-let msg = document.getElementById("msg");
-let posts = document.getElementById("posts");
+// Get the form, input field, message box, and post list
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const msg = document.getElementById("msg");
+const posts = document.getElementById("posts");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("button clicked");
-  
-    formValidation();
-  });
+// Add event listener for form submission
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevent default form submission behavior
 
-  let formValidation = () => {
-    if (input.value === "") {
-      msg.innerHTML = "Post cannot be blank";
-      console.log("failure");
-    } else {
-      console.log("successs");
-      msg.innerHTML = "";
-    }
-  };
+  // If the input is empty, display an error message
+  if (input.value.trim() === "") {
+    msg.innerHTML = "Post cannot be blank";
+    msg.classList.add("error");
+    input.classList.add("error");
+  } else {
+    // Otherwise, add the new post to the post list and clear the input field
+    msg.innerHTML = "";
+    msg.classList.remove("error");
+    input.classList.remove("error");
+    addPost(input.value.trim());
+    input.value = "";
+  }
+});
+
+// Function to add a new post to the post list
+const addPost = (postText) => {
+  const postItem = document.createElement("div");
+  postItem.classList.add("post-item");
+
+  const postContent = document.createElement("p");
+  postContent.innerText = postText;
+  postItem.appendChild(postContent);
+
+  const postOptions = document.createElement("span");
+  postOptions.classList.add("options");
+
+  const editIcon = document.createElement("i");
+  editIcon.classList.add("fas", "fa-edit");
+  postOptions.appendChild(editIcon);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("fas", "fa-trash-alt");
+  postOptions.appendChild(deleteIcon);
+
+  postItem.appendChild(postOptions);
+
+  posts.appendChild(postItem);
+};
